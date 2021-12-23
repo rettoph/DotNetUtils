831add19d21432ca5036ce18b10ff353d1e437c3
rettoph <anthony@turner.ec>
2021-12-23 13:42:09 -0700

    Update reflection helper methods

--- src/Minnow.System.Extensions/Reflection/MemberInfoExtensions.cs
+++ src/Minnow.System.Extensions/Reflection/MemberInfoExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 
-namespace Guppy.Extensions.System.Reflection
+namespace System.Reflection
 {
     public static class MemberInfoExtensions
     {
@@ -31,5 +31,27 @@ namespace Guppy.Extensions.System.Reflection
         public static TAttribute GetAttribute<TAttribute>(this MemberInfo member, Func<TAttribute, Boolean> filter = null, Boolean inherit = true)
             where TAttribute : Attribute
                 => member.GetCustomAttributes(inherit).FirstOrDefault(a => typeof(TAttribute).IsAssignableFrom(a.GetType()) && (filter?.Invoke(member as TAttribute) ?? true)) as TAttribute;
+
+        /// <summary>
+        /// Attempt to get the defined attribute, if it exists.
+        /// </summary>
+        /// <typeparam name="TAttribute"></typeparam>
+        /// <param name="member"></param>
+        /// <param name="attribute"></param>
+        /// <param name="filter"></param>
+        /// <param name="inherit"></param>
+        /// <returns></returns>
+        public static Boolean TryGetAttribute<TAttribute>(this MemberInfo member, out TAttribute attribute, Func<TAttribute, Boolean> filter = null, Boolean inherit = true)
+            where TAttribute : Attribute
+        {
+            if(member.HasCustomAttribute<TAttribute>(filter, inherit))
+            {
+                attribute = member.GetAttribute<TAttribute>(filter, inherit);
+                return true;
+            }
+
+            attribute = default;
+            return false;
+        } 
     }
 }
